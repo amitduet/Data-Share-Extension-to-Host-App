8a1e69b44c1c046ab101d589da3f81f64cfa9fdf
Amit Chowdhury <amit@nestedapps.com>
2016-11-02 11:12:23 +0600

    initial Commit

---
+++ MessageExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  MessageExtension
+//
+//  Created by Amit Chowdhury on 11/2/16.
+//  Copyright © 2016 Amit Chowdhury. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
